fa689f0463872f6219603b2ddbe9d6b2ec2f851c
Jason van Zyl <jason@tesla.io>
2013-08-01 10:55:19 -0400

    o adding the EPL license headers

--- product.rb
+++ product.rb
@@ -1,5 +1,11 @@
 #!/usr/bin/env ruby
 
+# Copyright (c) 2013 to Jason van Zyl
+# All rights reserved. This program and the accompanying materials                                                                                                                                          
+# are made available under the terms of the Eclipse Public License v1.0                                                                                                                                     
+# which accompanies this distribution, and is available at                                                                                                                                                  
+# http://www.eclipse.org/legal/epl-v10.html   
+
 require 'mustache'
 require 'rexml/document'
 require 'yaml'
